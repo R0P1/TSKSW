19d93b9598ce7641f00eef6f8beb7c427a7d521d
R0P1 <139617581+R0P1@users.noreply.github.com>
2023-09-03 21:52:20 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,12 +3,9 @@ import re
 import json
 import datetime
 
-# Mendapatkan timestamp saat ini
 sekarang = datetime.datetime.now()
-nama_file = f"hasil_wifi_{sekarang.strftime('%Y-%m-%d_%H-%M-%S')}.json"
-
+nama_file = f"hasil_wifi_{sekarang.strftime('%d-%m-%Y_%H-%M-%S')}.json"
 hasil_perintah = subprocess.run(["netsh", "wlan", "show", "profiles"], capture_output=True).stdout.decode()
-
 nama_profil = re.findall("All User Profile     : (.*)\r", hasil_perintah)
 
 daftar_wifi = []
@@ -17,26 +14,30 @@ if len(nama_profil) != 0:
     for nama in nama_profil:
         profil_wifi = {}
         info_profil = subprocess.run(["netsh", "wlan", "show", "profil", nama], capture_output=True).stdout.decode()
+        
         if re.search("Security key           : Absent", info_profil):
             continue
+       
         else:
             profil_wifi["ssid"] = nama
             info_kunci_pass = subprocess.run(["netsh", "wlan", "show", "profil", nama, "key=clear"], capture_output=True).stdout.decode()
             kata_sandi = re.search("Key Content            : (.*)\r", info_kunci_pass)
+            
             if kata_sandi is None:
                 profil_wifi["kata_sandi"] = None
+            
             else:
                 profil_wifi["kata_sandi"] = kata_sandi[1]
+            
             daftar_wifi.append(profil_wifi)
 
-# Mengkonversi daftar_wifi menjadi format JSON
-json_hasil = json.dumps(daftar_wifi, indent=4)
+hasil_json = json.dumps(daftar_wifi, indent=4)
 
-print(json_hasil)
+print(hasil_json)
 
 # Menyimpan hasil JSON ke dalam file
 with open(nama_file, 'w') as file:
-    file.write(json_hasil)
+    file.write(hasil_json)
 
-print(f"Hasil telah disimpan dalam file: {nama_file}")
+print(f"\nHasil telah disimpan dalam file: {nama_file}")
             
