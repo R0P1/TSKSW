9ecec61c59dbe138e79b781ec14db48fe1e21a66
R0P1 <139617581+R0P1@users.noreply.github.com>
2023-09-03 23:09:31 +0700

    Update main.py

--- main.py
+++ main.py
@@ -16,21 +16,15 @@ nama_profil = re.findall("All User Profile     : (.*)\r", hasil_perintah)
 daftar_wifi = []
 
 if len(nama_profil) != 0:
-
     for nama in nama_profil:
         info_profil = subprocess.run(["netsh", "wlan", "show", "profil", nama], capture_output=True).stdout.decode()
-
         if re.search("Security key           : Absent", info_profil):
             continue
-
         else:
-            info_kunci_pass = subprocess.run(["netsh", "wlan", "show", "profil", nama, "key=clear"],
-                                             capture_output=True).stdout.decode()
+            info_kunci_pass = subprocess.run(["netsh", "wlan", "show", "profil", nama, "key=clear"], capture_output=True).stdout.decode()
             kata_sandi = re.search("Key Content            : (.*)\r", info_kunci_pass)
-
             if kata_sandi is None:
                 daftar_wifi.append(f"SSID: {nama}, Kata Sandi: None")
-
             else:
                 daftar_wifi.append(f"""SSID: {nama}
 Kata Sandi: {kata_sandi[1]}
